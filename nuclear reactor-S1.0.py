5e3105eb253070b4a5e9245c771d3ff5f81427a0
TTBE13 <153487590+TTBE13@users.noreply.github.com>
2023-12-12 09:11:11 +0300

    Nuclear Reactor S1.0
    Exiting beta testing, added fuel

---
+++ nuclear reactor-S1.0.py
@@ -0,0 +1,43 @@
+import time
+import random
+
+topl = 100
+val = 30
+timet = 0
+minpl = 'r'
+e = 0
+t = 0
+temp = 20
+e = int(input("Выбирите сложность(1-легко(остальные будут добавлены в будущем))>>"))
+if e == 1:
+	i = 1
+	while True:
+		time.sleep(0.3)
+		t = t + 2
+		temp = temp + i
+		val = val + 1
+		topl = topl - 1
+		print("температура реактора", temp, "и", topl, "% топлива")
+		if t == 10:
+			minpl = input("Введите 'y' чтобы понизить температуру реактора или 't' чтобы повысить температуру реактора или 'u' чтобы купить топливо>>")
+			t = t - 10
+		if minpl == 'y':
+			i = i - 2
+			print("температура реактора понижается")
+		if minpl == 't':
+			i = i + 2
+			print("температура реактора повышается")
+		if minpl == 'u':
+			val = val - 5
+			topl = topl + 10
+			print("у тебя осталось", val, "денег и", topl, "% топлива")
+			minpl = 'l'
+		if topl < 1:
+			print("вы проиграли, в реакторе не осталось топлива")
+			break
+		if temp > 1500:
+			print("вы проиграли, реактор взорвался")
+			break
+		if temp < 0:
+			print("вы проиграли, реактор выключился")
+			break
